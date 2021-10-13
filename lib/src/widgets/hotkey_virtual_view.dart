a67e9209e99a3b2e3b163d947179b68dfd23092e
LiJianying <lijy91@foxmail.com>
2021-10-13 21:48:17 +0800

    Update hotkey_virtual_view.dart

--- lib/src/widgets/hotkey_virtual_view.dart
+++ lib/src/widgets/hotkey_virtual_view.dart
@@ -14,18 +14,15 @@ class _VirtualKeyView extends StatelessWidget {
     return Container(
       padding: EdgeInsets.only(left: 5, right: 5, top: 3, bottom: 3),
       decoration: BoxDecoration(
-        color: Color(0xfffafbfc),
-        border: Border(
-          left: BorderSide(color: const Color(0xffc6cbd1), width: 1),
-          right: BorderSide(color: const Color(0xffc6cbd1), width: 1),
-          top: BorderSide(color: const Color(0xffc6cbd1), width: 1),
-          bottom: BorderSide(color: const Color(0xffc6cbd1), width: 1),
+        color: Theme.of(context).canvasColor,
+        border: Border.all(
+          color: Theme.of(context).dividerColor,
+          width: 1,
         ),
-        // border: Border.all(color: Color(0xffc6cbd1), width: 1),
         borderRadius: BorderRadius.circular(3),
         boxShadow: <BoxShadow>[
           BoxShadow(
-            color: const Color(0xff959da5),
+            color: Colors.black.withOpacity(0.3),
             offset: Offset(0.0, 1.0),
           ),
         ],
@@ -33,6 +30,7 @@ class _VirtualKeyView extends StatelessWidget {
       child: Text(
         keyLabel,
         style: TextStyle(
+          color: Theme.of(context).textTheme.bodyText2?.color,
           fontSize: 12,
         ),
       ),
