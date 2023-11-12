7ee0381f89191ba587ec55dfd6db8f07a519bf38
Ben Hagen <ben@ottomatic.io>
2023-11-12 09:03:14 +0100

    Fix for Nil Unwrapping

--- macos/Classes/HotkeyManagerPlugin.swift
+++ macos/Classes/HotkeyManagerPlugin.swift
@@ -30,39 +30,68 @@ public class HotkeyManagerPlugin: NSObject, FlutterPlugin {
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    public func register(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        let args:[String: Any] = call.arguments as! [String: Any]
-        
-        let keyCode = args["keyCode"] as! String
-        let modifiers = args["modifiers"] as! Array<String>
-        let identifier = args["identifier"] as! String
-        
-        self.hotKeyDict[identifier] = HotKey(
-            key: Key.init(pluginKeyCode: keyCode)!,
+
+     public func register(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: Any],
+              let keyCode = args["keyCode"] as? String,
+              let modifiers = args["modifiers"] as? Array<String>,
+              let identifier = args["identifier"] as? String else {
+            result(
+                FlutterError(
+                    code: "INVALID_ARGUMENTS",
+                    message: "Invalid arguments received for registering hotkey.",
+                    details: "Expected 'keyCode', 'modifiers', and 'identifier' in arguments but received: \(call.arguments ?? "nil")"
+                )
+            )
+            return
+        }
+
+        guard let key = Key.init(pluginKeyCode: keyCode) else {
+            result(
+                FlutterError(
+                    code: "INVALID_KEY_CODE",
+                    message: "Failed to initialize key with provided keyCode.",
+                    details: "The keyCode '\(keyCode)' could not be converted into a valid Key. Ensure that the keyCode is valid and corresponds to a recognizable key."
+                )
+            )
+            return
+        }
+
+        let hotKey = HotKey(
+            key: key,
             modifiers: NSEvent.ModifierFlags.init(pluginModifiers: modifiers)
         )
-        self.hotKeyDict[identifier]!.keyDownHandler = {
+
+        hotKey.keyDownHandler = {
             let arguments: NSDictionary = call.arguments as! NSDictionary
             self.channel.invokeMethod("onKeyDown", arguments: arguments, result: nil)
         }
-        self.hotKeyDict[identifier]!.keyUpHandler = {
+        hotKey.keyUpHandler = {
             let arguments: NSDictionary = call.arguments as! NSDictionary
             self.channel.invokeMethod("onKeyUp", arguments: arguments, result: nil)
         }
+
+        self.hotKeyDict[identifier] = hotKey
         result(true)
     }
-    
+
     public func unregister(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        let args:[String: Any] = call.arguments as! [String: Any]
+        guard let args = call.arguments as? [String: Any],
+              let identifier = args["identifier"] as? String else {
+            result(
+                FlutterError(
+                    code: "INVALID_ARGUMENTS",
+                    message: "Failed to unregister hotkey. Missing or invalid 'identifier' argument.",
+                    details: "Expected a valid 'identifier' but received: \(call.arguments ?? "nil")"
+                )
+            )
+            return
+        }
         
-        let identifier = args["identifier"] as! String
-        
-        self.hotKeyDict[identifier] = nil;
-        
+        self.hotKeyDict[identifier] = nil
         result(true)
     }
-    
+
     public func unregisterAll(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         self.hotKeyDict.removeAll();
         result(true)
